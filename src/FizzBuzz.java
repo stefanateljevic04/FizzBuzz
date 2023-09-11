209f4a05b1603ef2a3cf3ef7ffc4dff03a417d03
Stefan Ateljevic <stefanateljevic@Stefans-MacBook-Pro.local>
2023-09-11 19:28:08 -0400

    updates

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -13,7 +13,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
@@ -38,5 +38,8 @@ class FizzBuzz {
         }
         i++;
         return i;
+    }
+
+    public static void Main() {
     }
 }
